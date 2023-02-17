56a83ca81ef167ce517a95f5568a0d0894b6be42
Julius Handstein <jhandstein@ixto.de>
2023-02-17 14:57:59 +0100

    create check for environment variables

--- FlaskDemo/azure-ls-app.py
+++ FlaskDemo/azure-ls-app.py
@@ -11,19 +11,21 @@ def index():
 def manual_text_input():
     text_input = request.get_json()
     category, confidence = ls_prediction(text_input['text'])
-    output = {'text': f'Your input was classified as: *{category}* with confidence score: *{confidence}*'}
+    output = {'classification': f'Your input was classified as: *{category}* with confidence score: *{confidence}*'}
     return jsonify(output), 200
 
 @app.route('/entityrecog/', methods=['POST'])
 def entity_recognition():
     text_input = request.get_json()
-    output = ls_entity_recognition(text_input['text'])
+    entities = ls_entity_recognition(text_input['text'])
+    output = {'entities': entities}
     return jsonify(output), 200
 
 @app.route('/questionanswering/', methods=['POST'])
 def question_answering():
     text_input = request.get_json()
-    output = ls_question_answering(text_input['text'])
+    answer = ls_question_answering(text_input['text'])
+    output = {'answer': answer}
     return jsonify(output), 200
 
 if __name__ == '__main__':
--- FlaskDemo/call_azure_endpoint.py
+++ FlaskDemo/call_azure_endpoint.py
@@ -3,11 +3,20 @@ from dotenv import load_dotenv
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.textanalytics import TextAnalyticsClient, RecognizeCustomEntitiesAction
 from azure.ai.language.questionanswering import QuestionAnsweringClient
-from azure.ai.language.questionanswering import models as qna
 
 load_dotenv()
 
+def check_env_vars(project_vars) -> None:
+    var_names = project_vars + ["AZURE_LANGUAGE_ENDPOINT", "AZURE_LANGUAGE_KEY"]
+    for var in var_names:
+        if var not in os.environ:
+            raise OSError(f"Environment variable {var} not found. Please set it and try again.")
+
+
 def ls_prediction(input_text) -> tuple:
+
+    prediction_vars = ["CUSTOM_CLASSIFICATION_PROJECT_NAME", "CUSTOM_CLASSIFICATION_DEPLOYMENT_NAME"]
+    check_env_vars(prediction_vars)
 
     endpoint = os.environ["AZURE_LANGUAGE_ENDPOINT"]
     key = os.environ["AZURE_LANGUAGE_KEY"]
@@ -27,25 +36,23 @@ def ls_prediction(input_text) -> tuple:
 
     document_results = poller.result()
 
-    for doc, classification_result in zip(input_text, document_results):
+    for doc, classification_result in zip([input_text], document_results):
         if classification_result.kind == "CustomDocumentClassification":
             classification = classification_result.classifications[0]
-            # print("The document text '{}' was classified as '{}' with confidence score {}.".format(
-            #     doc, classification.category, classification.confidence_score))
+            print("The document text '{}' was classified as '{}' with confidence score {}.".format(
+                doc, classification.category, classification.confidence_score))
             return classification.category, classification.confidence_score
 
         elif classification_result.is_error is True:
             # print("Document text '{}' has an error with code '{}' and message '{}'".format(
             #     doc, classification_result.error.code, classification_result.error.message
             # ))
-            return 'classification error', 424
+            return 'classification error', classification_result.error.code
 
 def ls_entity_recognition(input_text: str) -> dict:
 
-    # endpoint = "https://openai-classification-jh-ls.cognitiveservices.azure.com/"
-    # key = "ae7af333850f4b91b73aecbccb926d0b"
-    # project_name = "openai-named-entity-jh"
-    # deployment_name = "entity-recog-test"
+    recognition_vars = ["CUSTOM_ENTITIES_PROJECT_NAME", "CUSTOM_ENTITIES_DEPLOYMENT_NAME"]
+    check_env_vars(recognition_vars)
 
     endpoint = os.environ["AZURE_LANGUAGE_ENDPOINT"]
     key = os.environ["AZURE_LANGUAGE_KEY"]
@@ -79,15 +86,13 @@ def ls_entity_recognition(input_text: str) -> dict:
             return entities
         else:
             print("There was an error with the request.")
-            return 'entity recognition error', 0
+            return 'entity recognition error', custom_entities_result.error.code
             
 
 def ls_question_answering(input_text: str) -> tuple:
-    
-    # endpoint = "https://openai-classification-jh-ls.cognitiveservices.azure.com/"
-    # key = "ae7af333850f4b91b73aecbccb926d0b"
-    # project_name = "openai-custom-questions"
-    # deployment_name = "production"
+
+    answering_vars = ["CUSTOM_QUESTIONS_PROJECT_NAME", "CUSTOM_QUESTIONS_DEPLOYMENT_NAME"]
+    check_env_vars(answering_vars)
 
     endpoint = os.environ["AZURE_LANGUAGE_ENDPOINT"]
     key = os.environ["AZURE_LANGUAGE_KEY"]
@@ -112,10 +117,14 @@ def ls_question_answering(input_text: str) -> tuple:
             best_candidate = [a for a in output.answers if a.confidence and a.confidence >= 0.2][0]
             print("Q: {}".format(input_text))
             print("A: {}".format(best_candidate.answer))
-            return best_candidate.answer, 0
+            return best_candidate.answer
         else:
             print(f"No answers found for '{input_text}'.")
-            return 'no answer found', 404
+            return 'no answer found', 424
 
 if __name__ == '__main__':
-    print(ls_question_answering('Winter Berlin'))
+    # print(ls_question_answering('Winter Berlin'))
+    if 'AZURE' in os.environ:
+        print('WORKS')
+    else:
+        print('ERROR')
